7fbe6630f27c11111b8abf09d8f0bab886dc2981
mateus <mateussergio309@gmail.com>
2022-07-21 13:53:20 -0300

    onclick

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+
+
+struct CardView: View{
+    var emojis = ["✈️","🚀","🚁","⛵️"]
+    @State var isFaceUp: Bool = true
+    var body: some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill()
+                shape.foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️")
+                    .font(.largeTitle)
+            }else{
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
+
